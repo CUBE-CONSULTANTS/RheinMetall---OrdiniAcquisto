1be5769c8e81c0cb5997602207811a418928cbd4
Lulle91 <125670291+Lulle91@users.noreply.github.com>
2024-03-04 12:52:29 +0100

    detail view, testata, tree table

--- ordini_acquisto/webapp/controller/Detail.controller.js
+++ ordini_acquisto/webapp/controller/Detail.controller.js
@@ -15,12 +15,72 @@ sap.ui.define([
       debugger
       this.userType = this.getOwnerComponent().getModel("userModel").getProperty("/tipoUtente");
       debugger;
-      // let objJSon = await fetch("/model/modMock.json");
-      // let data = await objJSon.json();
-      // let datiRichiesta = data.dati[1].posizioni
-      // let oModel= new JSONModel(datiRichiesta)
-      // this.setModel(oModel,"detailModel");          
+      this.getRouter().getRoute("Detail").attachPatternMatched(this._onRouteMatched, this);      
     },
+    _onRouteMatched: async function (oEvent) {
+      debugger
+      const orderId = oEvent.getParameter("arguments").Action;
+      const status = oEvent.getParameter("arguments").Status
+      let objJSon = await fetch("/model/modMock.json");
+      let data = await objJSon.json();
+      let orderDetail = data.dati.find(element => element.ordine === orderId)
+      let oModelDetail = new JSONModel(orderDetail);
+      this.setModel(oModelDetail, "detailModel");
+    },
+    onOpenTesti:function(oEvent){
+      this.setModel(new JSONModel(), "testiModel");
+      let oButton = oEvent.getSource()
+      let oView = this.getView()
+      let objSel = oEvent.getSource().getBindingContext("detailModel").getObject().testiPop
+      this.getModel("testiModel").setProperty("/", objSel);
+
+      if (!this._qPopover) {
+        this._pPopover = Fragment.load({
+          id: oView.getId(),
+          name: "ordiniacquisto.ordiniacquisto.view.Fragment.Detail.testi",
+          controller: this,
+        }).then(function (oPopover) {
+          oView.addDependent(oPopover);
+          let ogg = oView.getModel("testiModel").getData();
+          const oJsonModel = new JSONModel();
+          oPopover.setModel(oJsonModel, "testiPopover");
+          oPopover.getModel("testiPopover").setProperty("/", ogg);
+          return oPopover;
+        });
+      }
+      this._pPopover.then(function (oPopover) {
+        oPopover.openBy(oButton);
+      });        
+    },
+    onOpenAllegati:function(oEvent){
+      debugger       
+      let order = oEvent.getSource().getBindingContext("ordineModel").getObject().ordine
+      if(order === '6500000160'){
+        let oAllegatiMat = this.getModel("ordineModel").getData().allegati[0]
+        let oModel = new JSONModel(oAllegatiMat)        
+        this.setModel(oModel, "allegatiDialog");
+        this.onOpenDialog("pDialog","ordiniacquisto.ordiniacquisto.view.Fragment.Detail.listAllegati",this,"allegatiDialog")
+      }else if( order === '4500041619'){
+        let oAllegatiMat=  this.getModel("ordineModel").getData().allegati[1]
+        let oModel = new JSONModel(oAllegatiMat)        
+        this.setModel(oModel, "allegatiDialog");
+        this.onOpenDialog("pDialog","ordiniacquisto.ordiniacquisto.view.Fragment.Detail.listAllegati",this,"allegatiDialog")
+      } 
+    },
+    onCloseAllegati: function(oEvent){
+      oEvent.getSource().getParent().getParent().close()
+    },
+    onSelectAllegato: function (oEvent){
+      debugger
+      let src = oEvent.getSource().getBindingContext("allegatiDialog").getObject().src
+      let pdfModel = new JSONModel()
+      pdfModel.setProperty("/source",src)
+      this.setModel(pdfModel,"pdfModel")
+      this.onOpenDialog("lDialog","ordiniacquisto.ordiniacquisto.view.Fragment.Detail.allegato",this,"pdfModel")       
+      },
+      onCloseAllegati: function (oEvent){
+        oEvent.getSource().getParent().getParent().close()
+      },
     onNav: function(oEvent){
       this.getRouter().navTo("RouteHome")
     },
--- ordini_acquisto/webapp/controller/Home.controller.js
+++ ordini_acquisto/webapp/controller/Home.controller.js
@@ -23,7 +23,6 @@ sap.ui.define(
           debugger;
           let objJSon = await fetch("/model/modMock.json");
           let data = await objJSon.json();
-
           let oModel = new JSONModel(data);
           this.setModel(oModel, "ordineModel");
 
@@ -46,74 +45,12 @@ sap.ui.define(
         onCloseDialog: function(oEvent){
           oEvent.getSource().getParent().close()
         },
-        onCloseAllegati: function(oEvent){
-          oEvent.getSource().getParent().getParent().close()
-        },
-        onOpenTesti:function(oEvent){
-          debugger
-          this.setModel(new JSONModel(), "testiModel");
-          let oButton = oEvent.getSource()
-          let oView = this.getView()
-          let objSel = oEvent.getSource().getBindingContext("ordineModel").getObject().testiPop
-          this.getModel("testiModel").setProperty("/", objSel);
-
-          if (!this._qPopover) {
-            this._pPopover = Fragment.load({
-              id: oView.getId(),
-              name: "ordiniacquisto.ordiniacquisto.view.Fragment.testi",
-              controller: this,
-            }).then(function (oPopover) {
-              oView.addDependent(oPopover);
-              let ogg = oView.getModel("testiModel").getData();
-              const oJsonModel = new JSONModel();
-              oPopover.setModel(oJsonModel, "testiPopover");
-              oPopover.getModel("testiPopover").setProperty("/", ogg);
-              return oPopover;
-            });
-          }
-          this._pPopover.then(function (oPopover) {
-            oPopover.openBy(oButton);
-          });        
-        },
-        onOpenAllegati:function(oEvent){
-          debugger
-          
-          let order = oEvent.getSource().getBindingContext("ordineModel").getObject().ordine
-          if(order === '6500000160'){
-            let oAllegatiMat = this.getModel("ordineModel").getData().allegati[0]
-            let oModel = new JSONModel(oAllegatiMat)        
-            this.setModel(oModel, "allegatiDialog");
-            this.onOpenDialog("pDialog","ordiniacquisto.ordiniacquisto.view.Fragment.Detail.listAllegati",this,"allegatiDialog")
-          }else if( order === '4500041619'){
-            let oAllegatiMat=  this.getModel("ordineModel").getData().allegati[1]
-            let oModel = new JSONModel(oAllegatiMat)        
-            this.setModel(oModel, "allegatiDialog");
-            this.onOpenDialog("pDialog","ordiniacquisto.ordiniacquisto.view.Fragment.Detail.listAllegati",this,"allegatiDialog")
-          } 
-        },
-        onSelectAllegato: function (oEvent){
-          debugger
-          let src = oEvent.getSource().getBindingContext("allegatiDialog").getObject().src
-          let pdfModel = new JSONModel()
-          pdfModel.setProperty("/source",src)
-          this.setModel(pdfModel,"pdfModel")
-          this.onOpenDialog("lDialog","ordiniacquisto.ordiniacquisto.view.Fragment.allegato",this,"pdfModel")       
-          },
-          onCloseAllegati: function (oEvent){
-            oEvent.getSource().getParent().getParent().close()
-          },
           onOpenDetail: function(oEvent){
             debugger
-            this.getRouter().navTo("Detail")
-            // let oDatiMat = this.getModel("richiesteModel").getData().bom[0]
-            // let oModel = new JSONModel(oDatiMat)
-        
-            // if(oEvent.getParameter("cellControl").getBindingInfo("text").binding.getPath() === 'Materiale'){
-            //   if(oEvent.getParameter("cellControl").getBindingInfo("text").binding.getValue() === oDatiMat.Materiale){
-            //     this.setModel(oModel, "bomDialog");
-            //     this.onOpenDialog("nDialog","listardo.listardo.view.Fragment.bomDialog",this,"bomDialog")
-            //   }
-            // }
+            let orderId = oEvent.getParameter("rowContext").getObject().ordine
+            let status = oEvent.getParameter("rowContext").getObject().stato
+            status === 'sap-icon://status-positive' ? status = 'ok' : status = 'ko'
+            this.getRouter().navTo("Detail",{ Action: orderId, Status: status})
           },
           modDataConsegna: function (oEvent){
             debugger
